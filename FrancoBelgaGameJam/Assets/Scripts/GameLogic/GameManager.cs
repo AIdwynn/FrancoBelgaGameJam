6d65a6aa11e231adc9840e13566ef01d704d94ac
LAP2_DEMO\USER2_DEMO <whumpus.wu@gmail.com>
2024-02-28 12:23:57 +0100

    Gamemanager

--- FrancoBelgaGameJam/Assets/Scripts/GameLogic/GameManager.cs
+++ FrancoBelgaGameJam/Assets/Scripts/GameLogic/GameManager.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    public static GameManager Instance;
+
+    [SerializeField] int startIndex;
+    [SerializeField] List<Turn> turns = new List<Turn>();
+    int currentTurnIndex;
+    Turn currentTurn;
+    #endregion
+
+    #region Accessors
+    public Turn CurrentTurn { get { return currentTurn; } }
+    #endregion
+
+    #region MainLoop
+    private void Awake()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartTurn(true);
     }
+
+    private void Update()
+    {
+        if (currentTurn != null)
+            currentTurn.UpdateTurn();
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentTurn != null)
+            currentTurn.FixedUpdateTurn();
+    }
+    #endregion
+
+    #region Turn Start/End
+    public void StartTurn()
+    {
+        currentTurnIndex++;
+
+        currentTurn = turns[currentTurnIndex];
+
+        currentTurn.StartTurn();
+    }
+
+    public void StartTurn(bool dontIncrement)
+    {
+        if (dontIncrement)
+            currentTurnIndex++;
+
+        currentTurn = turns[currentTurnIndex];
+
+        currentTurn.StartTurn();
+    }
+
+    public void EndTurn()
+    {
+        if (currentTurnIndex >= turns.Count)
+        {
+            currentTurnIndex = 0;
+        }
+
+        StartTurn(true);
+    }
+    #endregion
 }
