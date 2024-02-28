fa7a336213e05ff61d7b2a25f6644f9b2363e2be
AIdwynn <vitaldewachter@gmail.com>
2024-02-28 11:45:28 +0100

    RoomScript Push

---
+++ FrancoBelgaGameJam/Assets/Script/RoomScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    [SerializeField] private Transform StartPoint;
+    [SerializeField] private Transform[] EndPoint;
+    
+    
+}
