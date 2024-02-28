a21f0b649de292ad25b3998f1b76f6060a0f3b39
JeroenGesquiere <jeroen.gesquiere@student.howest.be>
2024-02-28 15:53:36 +0100

    Changed Tag of Player Prefab

---
+++ FrancoBelgaGameJam/Assets/Script/Enemy.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.UI.GridLayoutGroup;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform _player;   
+    [SerializeField] private NavMeshAgent _agent;
+
+    [SerializeField] private float _detectionRange;
+    [SerializeField] private float _maxTravelDistance;
+    [SerializeField] private float _attackRange;
+
+    private float _distanceTraveled;
+    private float _previousRemainingDistance;
+
+    public bool IsStunned {  get; set; }
+
+    public void Activate()
+    {
+        _distanceTraveled = 0;
+
+        if (IsStunned) return; // if enemy is stunned then don't activate enemy
+
+        float distanceEnemyPlayer = Vector3.Distance(transform.position, _player.position);
+
+        if (distanceEnemyPlayer > _detectionRange) return;
+
+        if (distanceEnemyPlayer < _attackRange)
+        {
+            CheckAttackState();
+        }
+
+        else
+        {
+            // move normally
+            _agent.isStopped = false;
+
+            StartCoroutine(CheckMovement());
+
+        }
+    }
+
+    private IEnumerator CheckMovement()
+    {
+        while (_distanceTraveled < _maxTravelDistance)
+        {
+            _distanceTraveled += (_previousRemainingDistance - _agent.remainingDistance);
+
+            _previousRemainingDistance = _agent.remainingDistance;
+            yield return new WaitForEndOfFrame();
+        }
+
+        _agent.isStopped = true;
+    }
+
+    private void CheckAttackState()
+    {
+        Debug.Log("Is close enough to attack");
+    }
+
+    #region UnityMethods
+    private void Awake()
+    {
+        _player =
+
+    }
+    private void OnEnable()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        PrepareNextMove();
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _detectionRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _maxTravelDistance);
+    }
+    #endregion
+
+    private void PrepareNextMove()
+    {
+        _agent.isStopped = true;
+
+        NavMeshPath path = MakePath(transform.position, _player.position);
+        _agent.SetPath(path);
+
+        _previousRemainingDistance = _agent.remainingDistance;
+    }
+
+    private NavMeshPath MakePath(Vector3 startPos, Vector3 endPos)
+    {
+        NavMeshPath path = new NavMeshPath();
+
+        if(NavMesh.CalculatePath(startPos, endPos, NavMesh.AllAreas, path))
+        {
+            return path;
+        }
+        return path;
+    }
+
+    private float GetPathLength(NavMeshPath path)
+    {
+        float length = 0;
+
+        if ((path.status != NavMeshPathStatus.PathInvalid) && (path.corners.Length >= 1))
+        {
+            for (int i = 1; i < path.corners.Length; ++i)
+            {
+                length += Vector3.Distance(path.corners[i - 1], path.corners[i]);
+            }
+        }
+        return length;
+    }
+
+
+}
--- FrancoBelgaGameJam/Assets/Scripts/GameLogic/EnemyTurn.cs
+++ FrancoBelgaGameJam/Assets/Scripts/GameLogic/EnemyTurn.cs
@@ -1,12 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyTurn : Turn
 {
+    private Enemy[] _enemies = new Enemy[0];
+
+    private void Start()
+    {
+        GetAllEnemies();
+    }
+
+    private void GetAllEnemies()
+    {
+        _enemies = GameObject.FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+    }
+
     public override void StartTurn()
     {
-
+        foreach (Enemy enemy in _enemies)
+        {
+            enemy.enabled = true;
+            enemy.Activate();
+        }
     }
 
     public override void UpdateTurn()
